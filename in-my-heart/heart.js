d39b95234f31fa5dc390cf4f2796b8c55f0f1014
Eddine Djerboua <eddine.djerboua@gmail.com>
2021-04-16 12:08:42 +0200

    use location hash to parameterize controls

--- in-my-heart/heart.js
+++ in-my-heart/heart.js
@@ -170,7 +170,7 @@ function setControls (camera, domElement, deviceOrientationMode) {
 
 (function init () {
   const {scene, camera, renderer} = createScene()
-  const { controls } = setControls(camera, renderer.domElement, true)
+  const { controls } = setControls(camera, renderer.domElement, window.location.hash.includes('deviceOrientation'))
   const { vertices, trianglesIndexes} = useCoordinates()
   const { geo, material, heartMesh } = createHeartMesh(vertices, trianglesIndexes)
   scene.add(heartMesh)
